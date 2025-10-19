d672f4fbc53a84feb54004ad2c82c3719ea5af56
osman-koc <kocosm@hotmail.com>
2025-10-19 09:56:44 +0300

    feat: Add square logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="tr">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🔗</text></svg>" />
+        <link rel="icon" href="/square-logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/square-logo.svg" />
       </head>
       <body>
         {children}
